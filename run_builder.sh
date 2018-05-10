676f553cd3766888f6455fd87bfe571bac69b5f8
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-05-10 14:46:00 +0200

    install data_manager before running it

--- run_builder.sh
+++ run_builder.sh
@@ -30,6 +30,11 @@ docker run -d -v ${EXPORT_DIR}:/export/ -e GALAXY_CONFIG_GALAXY_DATA_MANAGER_DAT
 galaxy-wait -g ${GALAXY_URL}
 
 #TODO: make the yml file dynamic
+
+#CHANGED_YAML_FILES=${git diff --name-only $TRAVIS_COMMIT_RANGE -- '*.yml' '*.yaml'}
+
+shed-install -t data-managers/metaphlan2_download/tools.yaml -g ${GALAXY_URL} -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
+
 run-data-managers --config data-managers/metaphlan2_download/db_v20.yaml -g ${GALAXY_URL} -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
 
 ls -l ${DATA_MANAGER_DATA_PATH}
