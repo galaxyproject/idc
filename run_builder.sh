c6294c38245dec4543da8b2ed9b41e1dd4d2d007
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-05-10 14:21:33 +0200

    add initial build configs

---
+++ run_builder.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+: ${GALAXY_DOCKER_IMAGE:="quay.io/bgruening/galaxy:18.01"}
+: ${GALAXY_PORT:="8080"}
+: ${EPHEMERIS_VERSION:="0.8.0"}
+: ${GALAXY_DEFAULT_ADMIN_USER:="admin@galaxy.org"}
+: ${GALAXY_DEFAULT_ADMIN_PASSWORD:="admin"}
+: ${EXPORT_DIR:="$HOME/export/"}
+: ${DATA_MANAGER_DATA_PATH:="${EXPORT_DIR}/data_manager"}
+
+: ${PLANEMO_PROFILE_NAME:="wxflowtest"}
+: ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}
+
+GALAXY_URL="http://localhost:$GALAXY_PORT"
+
+if [ ! -f .venv ]; then
+    virtualenv .venv
+    . .venv/bin/activate
+    pip install -U pip
+    pip install ephemeris="${EPHEMERIS_VERSION}"
+fi
+
+echo 'ephemeris installed'
+
+. .venv/bin/activate
+
+mkdir -p ${DATA_MANAGER_DATA_PATH}
+
+docker run -d -v ${EXPORT_DIR}:/export/ -e GALAXY_CONFIG_GALAXY_DATA_MANAGER_DATA_PATH=/export/data_manager/ -p 8080:80 ${GALAXY_DOCKER_IMAGE}
+galaxy-wait -g ${GALAXY_URL}
+
+#TODO: make the yml file dynamic
+run-data-managers --config data-managers/metaphlan2_download/db_v20.yaml -g ${GALAXY_URL} -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
+
+ls -l ${DATA_MANAGER_DATA_PATH}
+
